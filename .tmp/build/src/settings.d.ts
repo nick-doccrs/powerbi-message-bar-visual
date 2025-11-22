10fae4c60351f6ddc97ed8ee561f6622f44f16c6
Nicholas Lea-Trengrouse <nick@doccrs.com>
2025-11-22 20:16:37 +0000

    added additional fonts

--- .tmp/build/src/settings.d.ts
+++ .tmp/build/src/settings.d.ts
@@ -1,6 +1,6 @@
 import { formattingSettings } from "powerbi-visuals-utils-formattingmodel";
 /**
- * Trigger card (matches objects.trigger in capabilities.json)
+ * Trigger card (objects.trigger)
  */
 export declare class TriggerSettingsCard extends formattingSettings.SimpleCard {
     name: string;
@@ -10,23 +10,24 @@ export declare class TriggerSettingsCard extends formattingSettings.SimpleCard {
     private sevItems;
     severityOnMatch: formattingSettings.ItemDropdown;
     severityOnNoMatch: formattingSettings.ItemDropdown;
-    slices: (formattingSettings.TextInput | formattingSettings.ToggleSwitch | formattingSettings.ItemDropdown)[];
+    slices: (formattingSettings.ItemDropdown | formattingSettings.ToggleSwitch | formattingSettings.TextInput)[];
 }
 /**
- * Text card (matches objects.text in capabilities.json)
+ * Text card (objects.text)
  */
 export declare class TextSettingsCard extends formattingSettings.SimpleCard {
     name: string;
     displayName: string;
-    fontFamily: formattingSettings.TextInput;
+    private fontItems;
+    fontFamily: formattingSettings.ItemDropdown;
     messageFontSize: formattingSettings.NumUpDown;
     detailFontSize: formattingSettings.NumUpDown;
     toggleFontSize: formattingSettings.NumUpDown;
     messageBold: formattingSettings.ToggleSwitch;
-    slices: (formattingSettings.TextInput | formattingSettings.NumUpDown | formattingSettings.ToggleSwitch)[];
+    slices: (formattingSettings.ItemDropdown | formattingSettings.NumUpDown | formattingSettings.ToggleSwitch)[];
 }
 /**
- * Root formatting model (controls what appears in the pane)
+ * Root formatting model
  */
 export declare class VisualFormattingSettingsModel extends formattingSettings.Model {
     trigger: TriggerSettingsCard;
